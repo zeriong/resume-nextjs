aeabf56cc9639e15359723e80d33cf7467a28d84
zeriong <jaeryong95@gmail.com>
2025-09-01 07:15:55 +0900

    refactor: intro Contact / Channel 부분 컴포넌트 분리

---
+++ src/components/intro/LinkList.jsx
@@ -0,0 +1,20 @@
+export default function LinkList({ list }) {
+  const { title, children } = list;
+  return (
+    <div className="flex flex-col gap-y-1.5">
+      <p className="font-bold text-xl text-primary">{title}</p>
+      {children?.map((item) => (
+        <div key={item.title}>
+          {`${item.title} `}
+          <a
+            href={item.href}
+            target="_blank"
+            className="ml-2 text-gray-700 font-medium"
+          >
+            {item.text}
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+}
