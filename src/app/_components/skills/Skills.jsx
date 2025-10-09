dd60e306440e453292c5cba64eb505ef4eb531cc
zeriong <jaeryong95@gmail.com>
2025-10-09 17:06:30 +0900

    refactor: projects, skills 컴포넌트 분기 및 배치치

---
+++ src/app/_components/skills/Skills.jsx
@@ -0,0 +1,53 @@
+import { Category } from "@/app/_components/common/Category";
+
+export const Skills = () => {
+  return (
+    <Category
+      name="Skills"
+      description={
+        <>
+          프로젝트에 사용 경험이 있는 기술들입니다.{" "}
+          <span className="border-b-2 border-blue-500">React</span>를 주력으로
+          서비스에 필요한 전반적인 과정을 경험하고 문제를 해결하며 이해하고 성장하는
+          데 중점을 두었습니다. 백엔드에서는{" "}
+          <span className="border-b-2 border-blue-500">NestJS</span> 사용에
+          익숙합니다.
+        </>
+      }
+    >
+      <div className="flex flex-col gap-y-[28px] mt-[32px] max-md:mt-[24px]">
+        <div className="flex gap-x-[52px] gap-y-[20px] max-md:flex-col">
+          <div className="flex flex-col w-[130px]">
+            <p className="text-gray-500 text-xl font-semibold leading-none">
+              Frontend
+            </p>
+          </div>
+          <div className="">
+            HTML/CSS, JavaScript, TypeScript, React, Redux Toolkit, React Query,
+            NextJS, Tailwindcss
+          </div>
+        </div>
+        <div className="flex gap-x-[52px] gap-y-[20px] max-md:flex-col">
+          <div className="flex flex-col w-[130px]">
+            <p className="text-gray-500 text-xl font-semibold leading-none">
+              Backend
+            </p>
+          </div>
+          <div className="flex flex-col gap-y-[44px]">
+            NodeJS, NestJS, Typeorm, JWT, Swagger, Mariadb
+          </div>
+        </div>
+        <div className="flex gap-x-[52px] gap-y-[20px] max-md:flex-col">
+          <div className="flex flex-col w-[130px]">
+            <p className="text-gray-500 text-xl font-semibold leading-none">Etc</p>
+          </div>
+          <div className="flex flex-col gap-y-[44px]">
+            Firebase, Openapi Generator, Linux Ubuntu, PM2,
+            <br />
+            Git/Github, WebStorm, DataGrip
+          </div>
+        </div>
+      </div>
+    </Category>
+  );
+};
